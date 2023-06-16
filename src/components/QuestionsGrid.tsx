12be4db609255ef67a7c9d347a2d87d643e0b27a
Bookuha <bookuha@gmail.com>
2023-06-16 21:32:30 +0300

    Refactor QuestionsGrid

--- src/components/QuestionsGrid.tsx
+++ src/components/QuestionsGrid.tsx
@@ -1,14 +1,22 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
 import QuestionPreviewCell from "./QuestionPreviewCell";
-import questions from "../data/questions";
+import { Question } from "../data/questions";
 
-const QuestionsGrid = () => {
+interface Props {
+  title: string;
+  questions: Question[];
+}
+
+const QuestionsGrid = ({ title, questions }: Props) => {
   return (
-    <SimpleGrid columns={{ sm: 5, md: 7, lg: 8, xl: 10 }}>
-      {questions.map((q) => (
-        <QuestionPreviewCell question={q} />
-      ))}
-    </SimpleGrid>
+    <Box paddingY="8px">
+      <Heading padding={2}>{title}</Heading>
+      <SimpleGrid columns={{ sm: 5, md: 7, lg: 8, xl: 10 }}>
+        {questions.map((q) => (
+          <QuestionPreviewCell question={q} />
+        ))}
+      </SimpleGrid>
+    </Box>
   );
 };
 
